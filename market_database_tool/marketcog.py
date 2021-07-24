554db4f54d10146363f4d28a52d63a60fe04ed87
johncarlo <antugaevasco@gmail.com>
2021-07-25 06:43:52 +0800

    added commad

--- market_database_tool/marketcog.py
+++ market_database_tool/marketcog.py
@@ -1,6 +1,6 @@
 import market_database_tool
 from discord.ext import commands
-import run
+import raia
 
 class MarketCommands(commands.Cog):
 	def __init__(self, bot):
@@ -9,18 +9,39 @@ class MarketCommands(commands.Cog):
 
 	@commands.command(name='checkitem')
 	async def checkitem(self, ctx, item_name_arg):
-		check = run.player_system.get_player_by_id(ctx.author.id)
+		check = raia.player_system.get_player_by_id(ctx.author.id)
 		if check == None:
 			await ctx.send(f"Hello there {ctx.author.mention} you cannot use any other commands because you are not yet in the game, type \"r/budgetme \" to get started.")
 			return
-		item = run.market_system.get_item_by_name(str(item_name_arg))
+		item = raia.market_system.get_item_by_name(str(item_name_arg))
 		await ctx.send(f"item id : {item.item_id}\nitem name : {item.item_name}\nitem price :  {item.item_price}")
+		return
 	
 
 	@commands.command(name='shop')
 	async def shop(self, ctx):
-		pass
-		
+		if check == None:
+			await ctx.send(f"Hello there {ctx.author.mention} you cannot use any other commands because you are not yet in the game, type \"r/budgetme \" to get started.")
+			return
+		raia.cache_items()
+		msg = ""
+		for key, value in raia.list_all_items():
+			msg+=f"{key}:{value}\n"
+		await ctx.send(msg)
+
+
+	@commands.command(name='buy')
+	async def buy(self, ctx, item_name):
+		check = raia.player_system.get_player_by_id(ctx.author.id)
+		if check == None:
+			await ctx.send(f"Hello there {ctx.author.mention} you cannot use any other commands because you are not yet in the game, type \"r/budgetme \" to get started.")
+			return
+		check_item = raia.market_system.get_item_by_name(str(item_name))
+		if check_item != None:
+			await ctx.send(f"{ctx.author.mention} bought {check_item.item_name} for {check_item.item_price}")
+		else:
+			return
+
 
 def setup(bot):
 	bot.add_cog(MarketCommands(bot))
--- player_database_tool/playercog.py
+++ player_database_tool/playercog.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import player_database_tool
-import run
+import raia
 
 class PlayerCommands(commands.Cog):
 	def __init__(self, bot):
@@ -10,20 +10,21 @@ class PlayerCommands(commands.Cog):
 
 	@commands.command(name="budgetme")
 	async def budget(self, ctx):
-		check = run.player_system.get_player_by_id(ctx.author.id)
+		check = raia.player_system.get_player_by_id(ctx.author.id)
 		if check == None:
-			run.player_system.new_player(ctx.author.id, str(ctx.author.name), 1000)
+			raia.player_system.new_player(ctx.author.id, str(ctx.author.name), 1000)
 			await ctx.send(f"Hello there {ctx.author.mention} here is 1000 to get you started!")
 		else:
 			return
-	
+
+
 	@commands.command(name="stat")
 	async def status(self, ctx, other_playername_arg=None):
-		check = run.player_system.get_player_by_id(ctx.author.id)
+		check = raia.player_system.get_player_by_id(ctx.author.id)
 		if check == None:
 			await ctx.send(f"Hello there {ctx.author.mention} you cannot use any other commands because you are not yet in the game, type \"r/budgetme \" to get started.")
 			return
-		player = run.player_system.get_player_by_id(ctx.author.id)
+		player = raia.player_system.get_player_by_id(ctx.author.id)
 		await ctx.send(f"stats:\nplayer name:{player.player_name}\nplayer gold:{player.player_gold}")
 
 
